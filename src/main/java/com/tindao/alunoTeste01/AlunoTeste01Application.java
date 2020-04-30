13fbe66bc0f904a92f65c1b87017dfb650192a7c
Manoel Tindão <timdao02@hotmail.com>
2020-04-30 12:51:23 -0300

    Criação do projeto de Teste 01

---
+++ src/main/java/com/tindao/alunoTeste01/AlunoTeste01Application.java
@@ -0,0 +1,13 @@
+package com.tindao.alunoTeste01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlunoTeste01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlunoTeste01Application.class, args);
+	}
+
+}
